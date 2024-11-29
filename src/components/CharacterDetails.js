1121e41e3a26d091b2fc68e58be607a7c8953ee2
Ayushi <a2vashis@uwaterloo.ca>
2024-11-29 21:14:20 +0530

    small changes

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -36,18 +36,18 @@ const CharacterDetails = () => {
       {loading? (<Loader/>): (
         <div className="character-details">
         <h1>{character.name}</h1>
-        <p><span>Height:</span> {character.height / 100} m</p>
-        <p><span>Mass:</span> {character.mass} kg</p>
-        <p><span>Birth Year:</span> {character.birth_year}</p>
-        <p><span>Added:</span> {new Date(character.created).toLocaleDateString("en-GB")}</p>
-        <p><span>Films:</span> {character.films.length}</p>
+        <p><span className="span-container">Height:</span> {character.height / 100} m</p>
+        <p><span className="span-container">Mass:</span> {character.mass} kg</p>
+        <p><span className="span-container">Birth Year:</span> {character.birth_year}</p>
+        <p><span className="span-container">Date person was added:</span> {new Date(character.created).toLocaleDateString("en-GB")}</p>
+        <p><span className="span-container">No. of films:</span> {character.films.length}</p>
         {homeworld && (
           <div>
             <h2>Homeworld</h2>
-            <p>Name: {homeworld.name}</p>
-            <p>Terrain: {homeworld.terrain}</p>
-            <p>Climate: {homeworld.climate}</p>
-            <p>Residents: {homeworld.residents.length}</p>
+            <p><span className="span-container">Name:</span> {homeworld.name}</p>
+            <p><span className="span-container">Terrain:</span> {homeworld.terrain}</p>
+            <p><span className="span-container">Climate:</span> {homeworld.climate}</p>
+            <p><span className="span-container">No. of residents:</span> {homeworld.residents.length}</p>
           </div>
         )}
         </div>
